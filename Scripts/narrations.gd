4f3da5738193c48602bf34d210f1f41bdaec45f2
Hadi Kaïs-Khan <kais-khan.hadi@4sh.fr>
2023-11-24 17:23:06 +0100

    Added new hud, and narrative dialogue

---
+++ Scripts/narrations.gd
@@ -0,0 +1,68 @@
+extends Node
+
+var narration1 = {
+	"played": false,
+	"dialogue":
+		[
+			{
+				"text": "Au commencement, il y eu le jour de la grande malédiction. Teils, comme tous les autres, fut frappé par la maladie",
+				"sound": preload("res://Assets/Sounds/bruit attaque 1.wav")
+			}
+		]
+}
+
+var narration2 = {
+	"played": false,
+	"dialogue":
+		[
+			{
+				"text": "Lorsqu'il découvrit sa première orbe de soin, il crût bien être sauvé...",
+				"sound": preload("res://Assets/Sounds/bruit attaque 1.wav")
+			}, 
+			{
+				"text": "Mais son espoir fût de courte durée, les orbes elles même êtaient frappées par la malédiction et perdaient peu à peu de leur aura.",
+				"sound": preload("res://Assets/Sounds/bruit attaque 1.wav")
+			},
+			{
+				"text": "Le temps lui manquait. Il lui fallait maintenant trouver un moyen d'échapper à ce monde...",
+				"sound": preload("res://Assets/Sounds/bruit attaque 1.wav")
+			}
+		]
+}
+
+var narration3 = {
+	"played": false,
+	"dialogue":
+		[
+			{
+				"text": "Quand il aperçut le portail, il crût être sauvé...",
+				"sound": preload("res://Assets/Sounds/bruit attaque 1.wav")
+			}, 
+			{
+				"text": "Mais il allait bientôt découvrir ce qui l'attendait de l'autre côté",
+				"sound": preload("res://Assets/Sounds/bruit attaque 1.wav")
+			}
+		]
+}
+
+var narration4 = {
+	"played": false,
+	"dialogue":
+		[
+			{
+				"text": "Un monde lugubre et angoissant. Mais la cure était quelque part dans ce monde, il le savait.",
+				"sound": preload("res://Assets/Sounds/bruit attaque 1.wav")
+			}, 
+			{
+				"text": "Mais prends garde Teils, car les anciennes pierres sont totalement corrompues, dans ce bas monde ...",
+				"sound": preload("res://Assets/Sounds/bruit attaque 1.wav")
+			}
+		]
+}
+
+var narrations = [
+	narration1,
+	narration2,
+	narration3,
+	narration4
+]
