352a809264cb85262a12cc551e8504e32411fdc2
Hadi Kaïs-Khan <kais-khan.hadi@4sh.fr>
2023-11-24 21:32:15 +0100

    Added footsteps for player & darkstone

---
+++ GameScenes/DarkStone/darkstone.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+@export var damage = 10
+@export var frequency = 0.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Sprite2D.self_modulate.a = 0.0
+	$Timer.wait_time = frequency
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		do_damage()
+		$Timer.start()
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		$Timer.stop()
+
+func flicker_sprite():
+	$Sprite2D.self_modulate.a = 0.0
+	create_tween().tween_property($Sprite2D, "self_modulate:a", 1.0, 0.1)
+	$Sprite2D.self_modulate.lerp(Color.WHITE, 0.2)
+	await get_tree().create_timer(0.2).timeout
+	create_tween().tween_property($Sprite2D, "self_modulate:a", 0.0, 0.1)
+
+func do_damage():
+	flicker_sprite()
+	get_tree().get_first_node_in_group("player").hit(damage)
+
+func _on_timer_timeout():
+	do_damage()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,27 +1,32 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-
 var sprite
 
 func _ready():
 	sprite = $PlayerSprite
 
-
 func _physics_process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * SPEED
 	if direction.length() > 0:
+		if $Timer.is_stopped() == true:
+			$AudioStreamPlayer2D.play()
+			$Timer.start()
 		sprite.animation = "walk"
 		sprite.flip_h = velocity.x < 0
 		sprite.play()
 	else:
+		$Timer.stop()
 		sprite.animation = "idle"
 		sprite.stop()
-		
+	
 	move_and_slide()
 
 
 func _on_worlds_can_toggle_world():
 	pass # Replace with function body.
+
+
+func _on_timer_timeout():
+	$AudioStreamPlayer2D.play()
