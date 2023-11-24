96adaf000bbaae26b975ec9b4c2ef0effc665668
Xavier Hanin <xavier.hanin@4sh.fr>
2023-11-24 22:17:58 +0100

    lvl3 wip

--- GameScenes/PickupItems/MentalHealthHealStone/MentalHealthHealStone.gd
+++ GameScenes/PickupItems/MentalHealthHealStone/MentalHealthHealStone.gd
@@ -5,7 +5,6 @@ extends Node2D
 
 func _on_area_2d_body_entered(body):
 	if (body.is_in_group("player")):
-		print("heal")
-	else:
-		print("no heal")
+		get_tree().get_first_node_in_group("player").mental_heal(heal)
+		queue_free()
 
