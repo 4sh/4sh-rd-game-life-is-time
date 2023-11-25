aa9a1fd5ff06604f7fa486e461b7a13a9bfbf5cf
Hadi Kaïs-Khan <kais-khan.hadi@4sh.fr>
2023-11-25 17:58:07 +0100

    Up down mental health stone

--- GameScenes/PickupItems/MentalHealthHealStone/MentalHealthHealStone.gd
+++ GameScenes/PickupItems/MentalHealthHealStone/MentalHealthHealStone.gd
@@ -2,18 +2,23 @@ extends Node2D
 
 @export var heal: int = 10
 
-var tween
-
 func _ready():
-	tween = create_tween()
-	while(true):
-		tween.tween_property($Sprite2D, "position:y", -5.0, 1.0)
-		await tween.finished
-		tween.tween_property($Sprite2D, "position:y", 0.0, 1.0)
-		await tween.finished
+	var tween =  create_tween()
+	tween.tween_property($Sprite2D, "position:y", -5.0, 0.5)
+	await tween.finished
+	var tween_2 = create_tween()
+	tween_2.tween_property($Sprite2D, "position:y", 0.0, 0.5)
+	await tween_2.finished
 
 func _on_area_2d_body_entered(body):
 	if (body.is_in_group("player")):
 		get_tree().get_first_node_in_group("player").mental_heal(heal)
 		queue_free()
 
+func _on_up_down_timer_timeout():
+	var tween =  create_tween()
+	tween.tween_property($Sprite2D, "position:y", -5.0, 0.5)
+	await tween.finished
+	var tween_2 = create_tween()
+	tween_2.tween_property($Sprite2D, "position:y", 0.0, 0.5)
+	await tween_2.finished
