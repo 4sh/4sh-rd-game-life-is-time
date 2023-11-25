b0e5057b40a9050514ee2783a0dc42f63f44f944
Xavier Hanin <xavier.hanin@4sh.fr>
2023-11-25 08:30:53 +0100

    improve level 1 start & finish camera

--- GameScenes/Levels/Level 1/Level1.gd
+++ GameScenes/Levels/Level 1/Level1.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 
 func _ready():
-	$Player/Camera2D.zoom = Vector2(0.2,0.2)
-	$Player/Camera2D.offset = Vector2(0,-200)
-	create_tween().tween_property($Player/Camera2D, 'zoom', Vector2(4, 4), 4)
+	$Player/Camera2D.zoom = Vector2(0.8,0.8)
+	$Player/Camera2D.offset = Vector2(0,-350)
+	create_tween().tween_property($Player/Camera2D, 'offset', Vector2(0, -200), 1.5)
+	await create_tween().tween_property($Player/Camera2D, 'zoom', Vector2(1.5, 1.5), 1.5).finished
 	create_tween().tween_property($Player/Camera2D, 'offset', Vector2(0, 0), 3)
+	create_tween().tween_property($Player/Camera2D, 'zoom', Vector2(4, 4), 3)
 
 
 func _process(delta):
@@ -25,6 +27,8 @@ func _on_hud_restart_game():
 
 func _on_portal_body_entered(body):
 	create_tween().tween_property($Player/Camera2D, 'zoom', Vector2(1, 1), 3)
+	create_tween().tween_property($Player/Camera2D, 'offset', Vector2(0, 100), 3)
+
 	$Player.paused = true
 	$World/DiseaseTimer.stop()	
 	await $Hud.write_finished
