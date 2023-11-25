3dd7ace173e7d07de4d2ed87e76ce13368b6a16b
Xavier Hanin <xavier.hanin@4sh.fr>
2023-11-25 17:17:52 +0100

    add level 4 end

--- GameScenes/Levels/Level 4/Level4.gd
+++ GameScenes/Levels/Level 4/Level4.gd
@@ -19,3 +19,15 @@ func _on_the_sword_area_body_entered(body):
 		$Player.heal(100)
 		$Hud.has_attack = true
 		$Worlds/Light/Items/TheSword.queue_free()
+
+
+func _on_the_end_area_body_entered(body):
+	if body.is_in_group("player"):
+		$Hud.help_text("Vous avez trouvé la porte vers ... l'autre côté")
+		$Player.paused = true
+		$Worlds/Light/DiseaseTimer.stop()	
+		create_tween().tween_property($Player/Camera2D, 'offset', Vector2(0, -500), 2.5)
+		await create_tween().tween_property($Player/Camera2D, 'zoom', Vector2(0.5, 0.5), 3).finished
+
+		get_tree().change_scene_to_file("res://GameScenes/Levels/Level 5/EnterLevel5.tscn")
+		
