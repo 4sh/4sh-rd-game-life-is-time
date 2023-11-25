781ec34b0a023b741e6e050219789f0b4e51cc83
Xavier Hanin <xavier.hanin@4sh.fr>
2023-11-25 09:08:08 +0100

    add cursed anim to level 1

--- GameScenes/Levels/Level 1/Level1.gd
+++ GameScenes/Levels/Level 1/Level1.gd
@@ -4,11 +4,21 @@ extends Node2D
 func _ready():
 	$Player/Camera2D.zoom = Vector2(0.8,0.8)
 	$Player/Camera2D.offset = Vector2(0,-350)
+	get_tree().create_timer(3.0).connect("timeout", Callable(self, "play_cursed"))
 	create_tween().tween_property($Player/Camera2D, 'offset', Vector2(0, -200), 1.5)
 	await create_tween().tween_property($Player/Camera2D, 'zoom', Vector2(1.5, 1.5), 1.5).finished
 	create_tween().tween_property($Player/Camera2D, 'offset', Vector2(0, 0), 3)
 	create_tween().tween_property($Player/Camera2D, 'zoom', Vector2(4, 4), 3)
 
+func play_cursed():
+	$Cursed.color.a = 0.0
+	$Cursed.visible = true
+	for n in 3:
+		await create_tween().tween_property($Cursed, 'color:a', 1.0, 0.25).finished
+		await create_tween().tween_property($Cursed, 'color:a', 0.0, 0.25).finished
+		await get_tree().create_timer(0.2).timeout
+	$Cursed.visible = false
+	
 
 func _process(delta):
 	pass
