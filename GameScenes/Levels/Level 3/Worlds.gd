c500b81a611e1e39eb0078ff97c333b529977d61
Xavier Hanin <xavier.hanin@4sh.fr>
2023-11-25 05:59:55 +0100

    fix toggle world detection

--- GameScenes/Levels/Level 3/Worlds.gd
+++ GameScenes/Levels/Level 3/Worlds.gd
@@ -35,13 +35,13 @@ func _process(delta):
 		old_world = dark_world
 		new_world = light_world
 			
-	var target_map_pos = new_world.map.local_to_map(player.position)
+	var target_map_pos = new_world.map.local_to_map(new_world.map.to_local(player.position))
 	var tile_data: TileData = new_world.map.get_cell_tile_data(1, target_map_pos)
 	
 	var can_toggle = tile_data != null
 
 	can_toggle_world.emit(can_toggle)
-		
+				
 	if (can_toggle && Input.is_action_just_pressed("toggle_dark_world")):
 		remove_child(old_world)
 		add_child(new_world)
