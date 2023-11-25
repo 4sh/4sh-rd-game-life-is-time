6b5992b09af0678e191e381b4a1e7ef11166ff23
Xavier Hanin <xavier.hanin@4sh.fr>
2023-11-25 17:34:21 +0100

    fixup l2

--- GameScenes/Levels/Level 2/Level2.gd
+++ GameScenes/Levels/Level 2/Level2.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _ready():
-	$Player.position = Vector2(530,545) # test level end
+	# $Player.position = Vector2(530,545) # test level end
 	pass
 
 func _process(delta):
@@ -22,6 +22,7 @@ func _on_hud_restart_game():
 func _on_sleep_travel_stone_body_entered(body):
 	if body.is_in_group("player"):
 		$Hud.has_toggle_world = true
+		$World/Items/SleepTravelStone.queue_free()
 		$Hud.help_text("Appuyer sur espace pour utiliser la pierre de sommeil")
 
 func _unhandled_input(event):
