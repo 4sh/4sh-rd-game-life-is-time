f062a88a20f27d3e51fa1cce03cdc4e77b0a9442
Hadi Kaïs-Khan <kais-khan.hadi@4sh.fr>
2023-11-25 17:50:39 +0100

    Mental health sprite

--- GameScenes/PickupItems/MentalHealthHealStone/MentalHealthHealStone.gd
+++ GameScenes/PickupItems/MentalHealthHealStone/MentalHealthHealStone.gd
@@ -2,6 +2,15 @@ extends Node2D
 
 @export var heal: int = 10
 
+var tween
+
+func _ready():
+	tween = create_tween()
+	while(true):
+		tween.tween_property($Sprite2D, "position:y", -5.0, 1.0)
+		await tween.finished
+		tween.tween_property($Sprite2D, "position:y", 0.0, 1.0)
+		await tween.finished
 
 func _on_area_2d_body_entered(body):
 	if (body.is_in_group("player")):
