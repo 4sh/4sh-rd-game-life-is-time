838b0953e8fada95d5e83063541abed330cc1774
Xavier Hanin <xavier.hanin@4sh.fr>
2023-11-26 17:58:06 +0100

    easier level4

--- GameScenes/Levels/Level 4/Level4.gd
+++ GameScenes/Levels/Level 4/Level4.gd
@@ -16,7 +16,7 @@ func _on_the_sword_area_body_entered(body):
 		$Hud.help_text("Vous avez trouvé l'épée mentale...")
 		$Player.can_attack = true
 		$Player.mental_heal(100)
-		$Player.heal(100)
+		$Player.heal(10)
 		$Hud.has_attack = true
 		$Worlds/Light/Items/TheSword.queue_free()
 
@@ -25,6 +25,7 @@ func _on_the_end_area_body_entered(body):
 	if body.is_in_group("player"):
 		$Hud.help_text("Vous avez trouvé la porte vers ... l'autre côté")
 		$Player.paused = true
+		$Player.invulnerable = true
 		$Worlds/Light/DiseaseTimer.stop()	
 		create_tween().tween_property($Player/Camera2D, 'offset', Vector2(0, -500), 2.5)
 		await create_tween().tween_property($Player/Camera2D, 'zoom', Vector2(0.5, 0.5), 3).finished
