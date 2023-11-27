adbf6c62e6939208279ab8053233156a9d1a906f
Xavier Hanin <xavier.hanin@4sh.fr>
2023-11-27 21:32:39 +0100

    make blob life configurable in battle tester

--- Scenes/draft_scenes/BattleTester.gd
+++ Scenes/draft_scenes/BattleTester.gd
@@ -3,6 +3,7 @@ extends Node2D
 const BLOB = preload("res://GameScenes/Blob/blob.tscn")
 @export var nb_blobs:int = 5
 @export var blobs_speed:int = 30
+@export var blobs_life:int = 20
 @export var blobs_max_distance:int = 50
 
 
@@ -12,6 +13,7 @@ func _ready():
 		blob.position = $Player.position + Vector2(100, n*20)
 		blob.max_distance = blobs_max_distance
 		blob.speed = blobs_speed
+		blob.life = blobs_life
 		add_child(blob)
 
 func _process(delta):
